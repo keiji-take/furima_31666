3041813128d95c5618b461b809be624665dc6a98
keiji-take <social.200512@gmial.com>
2020-11-13 15:16:31 +0900

    nicknameを8→6文字以内に変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, length: { maximum: 8 }
+  validates :nickname, presence: true, length: { maximum: 6 }
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birth_date, presence: true
-
 
 end
