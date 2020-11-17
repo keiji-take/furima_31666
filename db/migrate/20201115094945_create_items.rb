0bb955f3085326755a85151bbe667351a6068dc0
keiji-take <social.200512@gmial.com>
2020-11-17 14:00:54 +0900

    Update 20201115094945_create_items.rb

--- db/migrate/20201115094945_create_items.rb
+++ db/migrate/20201115094945_create_items.rb
@@ -4,9 +4,9 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.string :product, null: false
       t.text :exception, null: false
       t.integer :category_id, null: false
-      t.integer :condithon_id, null: false
+      t.integer :condition_id, null: false
       t.integer :payer_id, null: false
-      t.integer :lovation_id, null: false
+      t.integer :location_id, null: false
       t.integer :send_day_id, null: false
       t.integer :price, null: false
       t.references :user, null: false, foreign_key: true
