8f7db473a76462e25cc3ba8234211e0dc911e7a8
keiji-take <social.200512@gmial.com>
2020-11-18 19:03:26 +0900

    商品一覧機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,13 @@ class ItemsController < ApplicationController
   # before_action :current_user, only: [:edit, :update]
   before_action :authenticate_user!
   def index
+    @items = Item.includes(:user).order('created_at DESC')
   end
+
   def new
     @item = Item.new
   end
+
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -15,24 +18,25 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
   def edit
   end
+
   def update
   end
 
   private
+
   def item_params
     params.require(:item).permit(:product, :exception, :category_id, :condition_id, :payer_id, :location_id, :send_day_id, :price, :image).merge(user_id: current_user.id)
   end
+
   def unsigned_user
-    unless user_signed_in?
-      redirect_to root_path
-    end
+    redirect_to root_path unless user_signed_in?
   end
   # def current_user
   #   unless current_user.id == @item.user.id
   #     redirect_to root_path
   #   end
   # end
-  
 end
