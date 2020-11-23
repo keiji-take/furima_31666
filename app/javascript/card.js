bb50c38a69bc26e07e2df32c46d4f8dcb5d34f9b
keiji-take <social.200512@gmial.com>
2020-11-23 18:17:14 +0900

    クレジットカードの情報送信までのJs導入

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,7 +3,31 @@ const pay = () => {
   const form = document.getElementById("charge-form")
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("イベンと着火")
+
+    const formResult = document.getElementById("charge-form");
+    //FormDataオブジェクトとはサーバーにデータを送信するオブジェクト
+    const formData = new FormData(formResult);
+
+    const card = {
+      //変数cardに定義するオブジェクトの定義
+      number: formData.get("number"),
+      cvc: formData.get("cvc"),
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+    };
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type='hidden'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+      }
+      document.getElementById("number").removeAttribute("name");
+      document.getElementById("cvc").removeAttribute("name");
+      document.getElementById("exp_month").removeAttribute("name");
+      document.getElementById("exp_year").removeAttribute("name");
+      document.getElementById("charge-form").submit();
+    });
   });
 };
 window.addEventListener("load", pay)
