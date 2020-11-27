20be7190b170bf6c769026629e5d3ef8b4e79753
keiji-take <social.200512@gmial.com>
2020-11-27 17:31:01 +0900

    再編集02

--- spec/models/order_trade_spec.rb
+++ spec/models/order_trade_spec.rb
@@ -15,18 +15,43 @@ RSpec.describe OrderTrade, type: :model do
       end
     end
     context '商品購入ができないとき' do
-      it '郵便番号,住所,電話番号,クレジットカード情報が空とき' do
+      it '郵便番号がからのとき' do
         @order_trade.post_code = ''
+        @order_trade.valid?
+        expect(@order_trade.errors.full_messages).to include("Post code can't be blank")
+      end
+      it '都道府県が空のとき' do
         @order_trade.prefecture = ''
+        @order_trade.valid?
+        expect(@order_trade.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '市町村が空のとき' do
         @order_trade.city = ''
+        @order_trade.valid?
+        expect(@order_trade.errors.full_messages).to include("City can't be blank")
+      end
+      it '番地が空のとき' do
         @order_trade.house_num = ''
+        @order_trade.valid?
+        expect(@order_trade.errors.full_messages).to include("House num can't be blank")
+      end
+      it '電話番号が空のとき' do
         @order_trade.tel = ''
+        @order_trade.valid?
+        expect(@order_trade.errors.full_messages).to include("Tel can't be blank")
+      end
+      it 'クレジット情報が空のとき' do
         @order_trade.token = ''
         @order_trade.valid?
-        expect(@order_trade.errors.full_messages).to include("Post code can't be blank", 'Post code is invalid. Include hyphen(-)', "Prefecture can't be blank", "City can't be blank", "House num can't be blank", "Tel can't be blank", 'Tel is invalid', "Token can't be blank")
+        expect(@order_trade.errors.full_messages).to include("Token can't be blank")
       end
       it '郵便番号にハイフン(-)がないとき' do
         @order_trade.post_code = '1234567'
+        @order_trade.valid?
+        expect(@order_trade.errors.full_messages).to include('Post code is invalid. Include hyphen(-)')
+      end
+      it '郵便番号が8桁以上の数字とき' do
+        @order_trade.post_code = '11111111'
         @order_trade.valid?
         expect(@order_trade.errors.full_messages).to include('Post code is invalid. Include hyphen(-)')
       end
@@ -38,7 +63,7 @@ RSpec.describe OrderTrade, type: :model do
       it '電話番号が12桁以上の数字のとき' do
         @order_trade.tel = '11111111111111111111'
         @order_trade.valid?
-        expect(@order_trade.errors.full_messages).to include("Tel is too long")
+        expect(@order_trade.errors.full_messages).to include()
       end
     end
   end
