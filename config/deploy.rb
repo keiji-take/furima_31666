dec6c47f6648a70d5623c6ea4fc449b8f9d7066b
keiji-take <social.200512@gmial.com>
2021-02-03 15:44:00 +0900

    commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+# lock "~> 3.14.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+# set :application, "furima_31666"
+# set :repo_url, "git@github.com:keiji-take/furima_31666.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
